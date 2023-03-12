589446c1e4083b0f486c3d4db63ff9b0bb6a2f87
Sourcery AI <>
2023-03-12 22:46:24 +0000

    'Refactored by Sourcery'

--- Q4.py
+++ Q4.py
@@ -5,8 +5,7 @@ import sys
 import os
 
 
-V = 26507; #Vocabulary size
-
+V = 26507
 print("getting the dataset for training")
 
 labels = open('question-4-train-labels.csv',"r+")
@@ -20,38 +19,31 @@ featuresDatasetTrain = list(csvFile)
 print("start trainig the model")
 
 #parameters
-N = 0;#Number of emails
-N0 = 0;#Number of medical emails
-N1 = 0;#Number of space emails
-
-T0 = [0.]*V;#occurrences of the words in medical,we use float because we are going to divide numbers 
-T1 = [0.]*V;#occurrences of the words in space emails
-
-
-SUM0 = 0.0;#sum(words)in all medical emails
-SUM1 = 0.0;#sum(words) in all space emails
-
+N = 0
+N0 = 0
+N1 = 0
+T0 = [0.]*V
+T1 = [0.]*V
+SUM0 = 0.0
+SUM1 = 0.0
 for i in range(len(labelsDatasetTrain)):
-    if(labelsDatasetTrain[i][0]=='0'):
+    if (labelsDatasetTrain[i][0]=='0'):
         N0 = N0 + 1;
-        for j in range(0,V-1):
-           T0[j] = T0[j] + float(featuresDatasetTrain[i][j]);
-           SUM0 = SUM0 + float(featuresDatasetTrain[i][j])                         
-              
-    if(labelsDatasetTrain[i][0]=='1'):
-            N1 = N1 + 1;
-            for j in range(0,V-1):
-               T1[j] = T1[j] + float(featuresDatasetTrain[i][j]);
-               SUM1 = SUM1 + float(featuresDatasetTrain[i][j])              
+        for j in range(V-1):
+            T0[j] = T0[j] + float(featuresDatasetTrain[i][j]);
+            SUM0 = SUM0 + float(featuresDatasetTrain[i][j])                         
+
+    if (labelsDatasetTrain[i][0]=='1'):
+        N1 = N1 + 1;
+        for j in range(V-1):
+            T1[j] = T1[j] + float(featuresDatasetTrain[i][j]);
+            SUM1 = SUM1 + float(featuresDatasetTrain[i][j])
     N=N+1;
-    
-
-P0 = float(N0)/N;#probability of medical: class 0 
-P1 = float(N1)/N;#probability of space: class 0 
 
 
-
-for j in range(0,V-1):
+P0 = float(N0)/N
+P1 = float(N1)/N
+for j in range(V-1):
     T0[j] = float(T0[j])/SUM0;#prob of words j in medical class 0=medical
     T1[j] = float(T1[j])/SUM1;#prob of words j in space class 1=space
 print("finish trainig")
@@ -70,11 +62,11 @@ N0Test=0
 N1Test=0
 
 testLabes = []
-for i in range(len(labelsDatasetTest)):
-    if (labelsDatasetTest[i][0]=='0'):
+for item in labelsDatasetTest:
+    if item[0] == '0':
         testLabes.append(0)
         N0Test = N0Test +1
-    if(labelsDatasetTest[i][0]=='1'):
+    if item[0] == '1':
         testLabes.append(1)
         N1Test = N1Test +1
 
@@ -84,18 +76,18 @@ featuresDatasetTest = list(csvFile)
 
 
 testSize=len(featuresDatasetTest)
-PT0=[0.]*testSize; #Bayes Rule 
+PT0=[0.]*testSize
 PT1=[0.]*testSize;
 
 
-Prediction = [];    
+Prediction = [];
 for i in range(len(featuresDatasetTest)):
-    for j in range(0,V-1):
+    for j in range(V-1):
         if (T0[j] != 0):
             PT0[i] = math.log(T0[j]) * float(featuresDatasetTest[i][j]) + PT0[i] ;
         if (T1[j] != 0):
-            PT1[i] = math.log(T1[j]) * float(featuresDatasetTest[i][j]) + PT1[i] ; 
-    PT0[i] = math.log(P0) + PT0[i] 
+            PT1[i] = math.log(T1[j]) * float(featuresDatasetTest[i][j]) + PT1[i] ;
+    PT0[i] = math.log(P0) + PT0[i]
     PT1[i] = math.log(P1) + PT1[i]
 
     #we compare the two values to know which one maximize the value of the prob, to take decision for the class
@@ -104,14 +96,14 @@ for i in range(len(featuresDatasetTest)):
     if (PT1[i] >= PT0[i]) :
         Prediction.append(1)
 
-TN= 0; #predicting 1 and the real value is 1      
-TP= 0; #predicting 1 and the real value is 1   
+TN= 0
+TP= 0
 for i in range(len(featuresDatasetTest)):
-    if (Prediction[i] == testLabes[i]) and (testLabes[i] == 0):
-            TN=TN+1
-    if (Prediction[i] == testLabes[i]) and (testLabes[i] == 1):
-            TP=TP+1
-            
+    if Prediction[i] == testLabes[i] == 0:
+        TN=TN+1
+    if Prediction[i] == testLabes[i] == 1:
+        TP=TP+1
+
 CorrectPrediction = TP + TN;
 
 FP = N0Test - TN;
@@ -122,27 +114,24 @@ print("test set accuracy is :")
 print(accuracy)
 
 
-report = open('Result_Q4.txt', 'w')
-
-
-report.write("MLE estimator\n")
-report.write("\n" )
-report.write("The testing accuracy:")
-report.write(str(accuracy))
-report.write("\n" )
-report.write("Wrong predictions:\n")
-report.write("prediction medical , the real value is space :" )
-report.write(str (FP))
-report.write("\n" )
-report.write("prediction space , the real value is medical :" )
-report.write(str(MD))
-report.write("\n" )
-report.write("total false predictions: " )
-report.write(str(MD+FP))
-report.write("\n" )
-report.write("Size of the testing data: " )
-report.write(str(testSize))
-report.write("\n" )
-report.close()
+with open('Result_Q4.txt', 'w') as report:
+    report.write("MLE estimator\n")
+    report.write("\n" )
+    report.write("The testing accuracy:")
+    report.write(str(accuracy))
+    report.write("\n" )
+    report.write("Wrong predictions:\n")
+    report.write("prediction medical , the real value is space :" )
+    report.write(str (FP))
+    report.write("\n" )
+    report.write("prediction space , the real value is medical :" )
+    report.write(str(MD))
+    report.write("\n" )
+    report.write("total false predictions: " )
+    report.write(str(MD+FP))
+    report.write("\n" )
+    report.write("Size of the testing data: " )
+    report.write(str(testSize))
+    report.write("\n" )
 
     
--- Q6.py
+++ Q6.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 
-V = 26507; #Vocabulary size
+V = 26507
 print("getting dataset")
 labels = open('question-4-train-labels.csv',"r+")
 csvFile = csv.reader(labels,delimiter=',')
@@ -25,50 +25,50 @@ print("calculating MutualInf")
 
 MutualInf=[0.]*V;
 for i in range(len(labelsDatasetTrain)):
-    if(labelsDatasetTrain[i][0]=='0'):
-        for j in range(0,V-1):
+    if (labelsDatasetTrain[i][0]=='0'):
+        for j in range(V-1):
             if(int(featuresDatasetTrain[i][j]) != 0):               
                  N10[j] = N10[j] + 1 
             if(int(featuresDatasetTrain[i][j]) == 0):               
                  N00[j] = N00[j] + 1
-                 
-    if(labelsDatasetTrain[i][0]=='1'):
-        for j in range(0,V-1):
+
+    if (labelsDatasetTrain[i][0]=='1'):
+        for j in range(V-1):
             if(int(featuresDatasetTrain[i][j]) != 0):               
                  N11[j] = N11[j] + 1 
             if(int(featuresDatasetTrain[i][j]) == 0):               
-                 N01[j] = N01[j] + 1          
-for j in range(0,V-1):
+                 N01[j] = N01[j] + 1
+for j in range(V-1):
     N[j] = N11[j] + N10[j] + N00[j] + N01[j]
 
-for j in range(0,V-1):
+for j in range(V-1):
     if (N11[j] + N10[j] != 0)and(N11[j] + N01[j] != 0)and(N00[j] + N01[j]!= 0)and(N00[j] + N10[j] != 0):
         m1=(N[j]*N11[j])/((N11[j]+N10[j])*(N11[j]+N01[j]))
         m2=(N[j]*N01[j])/((N00[j]+N01[j])*(N11[j]+N01[j]))
         m3=(N[j]*N10[j])/((N11[j]+N10[j])*(N00[j]+N10[j]))
         m4=(N[j]*N00[j])/((N00[j]+N01[j])*(N00[j]+N10[j]))
-        
+
         MutualInf[j]=0
         if(m1 != 0):
             MutualInf[j] = ( N11[j]/ N[j] ) * math.log(m1,2)
-        if(m2 != 0):
-            MutualInf[j] = MutualInf[j] + ( N01[j]/N[j]) * math.log(m2, 2)
-        if(m3 != 0):
-            MutualInf[j] = MutualInf[j] + ( N10[j]/N[j]) * math.log(m3, 2)
-        if(m4 != 0):
-            MutualInf[j] = MutualInf[j] + ( N00[j]/N[j]) * math.log(m4, 2)
+        if (m2 != 0):
+            MutualInf[j] += ( N01[j]/N[j]) * math.log(m2, 2)
+        if (m3 != 0):
+            MutualInf[j] += ( N10[j]/N[j]) * math.log(m3, 2)
+        if (m4 != 0):
+            MutualInf[j] += ( N00[j]/N[j]) * math.log(m4, 2)
 
-   
+
 sortedMutualInf = [0.]*V
-for i in range(0,V-1):
+for i in range(V-1):
     sortedMutualInf[i] = MutualInf[i]
 
 indice = [0.]*V;
-for i in range(0,V-1):
+for i in range(V-1):
     indice[i] = i
 
 print("Sorting features by calculating mutual information")
-for i in range(0,V-1):
+for i in range(V-1):
     posmax=i
     indice[i] = i
     for j in range(i+1,V-1):
@@ -76,17 +76,16 @@ for i in range(0,V-1):
             posmax=j
     (indice[i],indice[posmax])=(indice[posmax],indice[i])           
     (sortedMutualInf[i],sortedMutualInf[posmax])=(sortedMutualInf[posmax],sortedMutualInf[i])
-    
+
 print("write result in Result_Q6.txt")
 
-result = open('Result_Q6.txt', 'w')
-result.write("Mutual information\n")
-result.write("Indices and mutual information scores of the 10 features in descending order\n")
-for i in range(0,10):
-    result.write("Feature N: ")
-    result.write(str (indice[i]))
-    result.write("  =>mutual information equal to: ")
-    result.write(str (sortedMutualInf[i]))
-    result.write("\n")
-result.close();
+with open('Result_Q6.txt', 'w') as result:
+    result.write("Mutual information\n")
+    result.write("Indices and mutual information scores of the 10 features in descending order\n")
+    for i in range(10):
+        result.write("Feature N: ")
+        result.write(str (indice[i]))
+        result.write("  =>mutual information equal to: ")
+        result.write(str (sortedMutualInf[i]))
+        result.write("\n")
 
